19b94bb349d1f86ffec41bf3a431f7005eefc1cd
seharuem <seharuem@naver.com>
2026-01-08 00:12:59 +0900

    최적화

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,37 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), svgr()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		svgr(),
+		visualizer({
+			open: true,
+			filename: 'stats.html',
+			gzipSize: true
+		})
+	],
+	build: {
+		rollupOptions: {
+			plugins: visualizer(),
+			output: {
+				manualChunks(id) {
+					if (id.includes('gsap')) {
+						return 'gsap';
+					}
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				}
+			}
+		}
+	},
+	optimizeDeps: {
+		include: ['gsap']
+	},
 	base: '/lhn_portfolio/'
 });
